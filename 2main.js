2097cc89bde2f6f6ffd81c1fa9f5c1e94e35c70d
agrah355 <74407072+agrah355@users.noreply.github.com>
2021-02-19 17:06:00 +0530

    Add files via upload

--- 2main.js
+++ 2main.js
@@ -1,6 +1,12 @@
 var user1=document.getElementById("user1_name").value;
 var user2=document.getElementById("user1_name").value;
 
+var answer_turn;
+var question_turn;
+
+var score1=document.getElementById("player1_score").innerHTML="";
+var score2=document.getElementById("player2_score").innerHTML="";
+
 document.getElementById("player1_name").innerHTML=user1;
 document.getElementById("player2_name").innerHTML=user2;
 
@@ -21,4 +27,26 @@ function send(){
 
     document.getElementById("number1").value="";
     document.getElementById("number2").value=""; 
+}
+
+function check(){
+    get_answer=document.getElementById("input_check_box").value;
+    if(get_answer==actule_number){
+        if(answer_turn=="user1"){
+            score1=score1+1;
+            document.getElementById("player1_score").innerHTML=score1;
+        }
+        else{
+            score2=score2+1;
+            document.getElementById("player2_score").innerHTML=score2;
+        }
+    }
+    if(question_turn=="user1"){
+        question_turn="user2";
+        document.getElementById("player_question").innerHTML="Question Turn: " +question_turn;
+    }
+    else{
+        question_turn="user1";
+        document.getElementById("player_question").innerHTML="Question Turn: " +question_turn;
+    }
 }
